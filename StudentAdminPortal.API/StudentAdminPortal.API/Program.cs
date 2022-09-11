e19724e56c7695769a3cdc45ef1dadf8aa9f5ff4
Elber Funez <elberfunez@gmail.com>
2022-09-11 17:40:06 -0400

    enabled cors

--- StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
@@ -10,6 +10,16 @@ builder.Services.AddControllers();
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("StudentAdminPortalDb");
 builder.Services.AddDbContext<StudentAdminContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddCors( (options) =>
+options.AddPolicy("angularApplication", (builder) =>
+{
+    builder.WithOrigins("http://localhost:4200")
+    .AllowAnyHeader()
+    .WithMethods("GET", "POST", "PUT", "DELETE")
+    .WithExposedHeaders("*");
+})
+);
 //inject dependencies inside the services
 builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
 
@@ -31,6 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("angularApplication");
 
 app.UseAuthorization();
 
